c9f23b9787dbb350a68cc4052a96a80616f2a6cf
rajthanusan <rajthanusan08@gmail.com>
2025-06-29 17:11:29 +0530

    #6, Backend Bugs Fixed

--- server/src/main/java/com/example/login/dto/LoginRequest.java
+++ server/src/main/java/com/example/login/dto/LoginRequest.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthResponse {
-    private String token;
+public class LoginRequest {
+    private String username;
+    private String password;
 }
